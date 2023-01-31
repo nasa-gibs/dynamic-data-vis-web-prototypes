e6f69f7f7c3f1aff62344a52278b559ff6298da8
ryanweiler92 <ryanweiler92@gmail.com>
2023-01-31 13:47:37 -0500

    updating with hash router and including /VEDA-Testing-Demo/ on homepage

--- VEDA-Testing-Demo/src/App.tsx
+++ VEDA-Testing-Demo/src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import myTheme from "./theme";
 import ApiTestingPage from "./pages/ApiTestingPage";
 import ImageryPage from "./pages/ImageryPage";
 
 export const App = () => (
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <ChakraProvider theme={myTheme}>
       <Routes>
         {/* <Route path="/gibs" element={<ImageryPage />} /> */}
         <Route path="/" element={<ApiTestingPage />} />
       </Routes>
     </ChakraProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
